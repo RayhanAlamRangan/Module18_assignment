d5b5a85d30d36962494ffbd40992bf2c42877082
MD.Rayhan Alam <140850073+RayhanAlamRangan@users.noreply.github.com>
2023-12-26 20:34:11 +0600

    commitchange

---
+++ index.js
@@ -0,0 +1,7 @@
+const app=require("./app");
+const PORT=5080;
+app.listen(PORT,function () {
+    console.log("App Run @5080")
+});
+
+
---
+++ src/models/CategoryModel.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+const DataSchema=mongoose.Schema({
+        categoryName:{type:String,unique:true,required:true},
+        categoryImg:{type:String,required:true}
+    },
+    {timestamps:true,versionKey:false}
+)
+const CategoryModel=mongoose.model('categories-products',DataSchema)
+module.exports=CategoryModel
